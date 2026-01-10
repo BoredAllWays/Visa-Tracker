05f9bce825589160862be7aff862720436a45792
BoredAllWays <saipranav64@gmail.com>
2026-01-09 20:32:21 -0500

    visa bulletin part finished

--- scripts/monte_carlo_improved.py
+++ scripts/monte_carlo_improved.py
@@ -6,6 +6,7 @@ from flatten_data import VisaDataProcessor
 from tqdm import tqdm
 import seaborn as sns
 
+
 def gen_sim_parameters(total_greencards = 140_000, country_cap = 0.07, category_preference = 0.286, n_years = 100):
     base_quota = total_greencards * country_cap * category_preference
     #spillovers
@@ -24,30 +25,40 @@ def gen_sim_parameters(total_greencards = 140_000, country_cap = 0.07, category_
     dependancy_ratio = np.random.triangular(left = 1.9, right = 3.0, mode = 1.9)
     duplicate_rate = np.random.uniform(0.05, 0.10)
     attrition_rates = [np.random.uniform(0.01, 0.04) for _ in range(n_years)]
-    return base_quota, spillover_list,attrition_rates, dependancy_ratio, duplicate_rate, 
+    return base_quota, spillover_list,attrition_rates, dependancy_ratio, duplicate_rate
+def gen_people_ahead(vdp, eb_inventory, td, l_inv_d, i_140_d, vbd, dupl, dep):
+    target_date = td
+    last_inv_date = l_inv_d
+    i_140_inventory_date = i_140_d
+    visa_bulletin_date = vbd
+    eb_inventory = eb_inventory[eb_inventory["Date"] > visa_bulletin_date]
+    if target_date < last_inv_date:
+        people_ahead = eb_inventory[(eb_inventory["Date"] < target_date)]["Count"].sum()
+        return people_ahead
+    people_ahead = eb_inventory["Count"].sum()
+    gap_days = (i_140_inventory_date - last_inv_date).days
+    gap_position_days = (target_date - last_inv_date).days
+    percent_in_gap = min(gap_position_days / gap_days, 1.0)
+    i_140_inventory_count = vdp.get_i140_snapshot() * percent_in_gap * (1-dupl) * dep
+    people_ahead += i_140_inventory_count
+    return people_ahead
 def monte_carlo(vdp, preference, country, sims=1000):
     #paremeters
     target_date = "2020-08-11"
+    visa_bulletin_date = "2023-02-23"
     target_date = pd.to_datetime(target_date)
     simulation_start_date = pd.Timestamp("2025-10-01")
     i_140_inventory_date = pd.Timestamp("2025-06-01")
     eb_inventory = vdp.flatten()
-    eb_inventory = eb_inventory[(eb_inventory["Visa Status"] == "Awaiting Availability") & (eb_inventory["Country Of Chargeability"] == country) & (eb_inventory["Preference Category"].str.contains(preference))].sort_values("Date")
+    eb_inventory = eb_inventory[(eb_inventory["Visa Status"] == "Awaiting Availability") & (eb_inventory["Country Of Chargeability"] == country) & (eb_inventory["Preference Category"].str.contains(preference)) & (eb_inventory["Count"] > 0)].sort_values("Date")
     last_inv_date = eb_inventory.iloc[-1]["Date"]
-    people_ahead = -1
-    #calculate year
+
     results = []
-    for _ in tqdm(range(sims), desc="running monte carlo simulations"):
+    #for _ in tqdm(range(sims), desc="running monte carlo simulations"):
+    for _ in range(sims):
         quota, spillovers, attr, dep, dupl = gen_sim_parameters()
-        if target_date > last_inv_date:
-            people_ahead = eb_inventory["Count"].sum()
-            gap_days = (i_140_inventory_date - last_inv_date).days
-            gap_position_days = (target_date - last_inv_date).days
-            percent_in_gap = min(gap_position_days / gap_days, 1.0)
-            i_140_inventory_count = vdp.get_i140_snapshot() * percent_in_gap * (1-dupl) * dep
-            people_ahead += i_140_inventory_count
-        else:
-            people_ahead = eb_inventory[eb_inventory["Date"] < target_date]["Count"].sum()
+        people_ahead = gen_people_ahead(vdp, eb_inventory, target_date, last_inv_date, i_140_inventory_date, visa_bulletin_date, dupl, dep)
+        print(eb_inventory[["Visa Status", "Preference Category", "Count"]])
         months_passed = 0
         current_backlog = people_ahead
         year_index = 0
@@ -91,10 +102,10 @@ def plot_histogram(results):
 
 def main():
     file_path = os.path.join(os.getcwd(), "data", "eb_inventory_october_2025.xlsx")
-    vdp = VisaDataProcessor(file_path, "India", "EB2")
-    results = monte_carlo(vdp, "EB2", "India", sims=1000)
+    vdp = VisaDataProcessor(file_path, "China", "1st")
+    results = monte_carlo(vdp, "1st", "China", sims=1000)
     print(results)
-    plot_histogram(results)
+    #plot_histogram(results)
 
 if __name__ == "__main__":
     main()
